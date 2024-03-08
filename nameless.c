f587a90561e5edf2724c782113da1bae61919bda
th3nn3ss <chuksmcdennis@yahoo.com>
2024-03-08 23:42:56 +0100

    capture window size for most terminal emulators out there

--- nameless.c
+++ nameless.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -59,11 +60,37 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+    return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1)
+      break;
+    if (buf[i] == 'R')
+      break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[')
+    return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+    return -1;
+
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+      return -1;
+    return getCursorPosition(rows, cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
@@ -71,10 +98,47 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/** append buffer */
+// Since C does not suppoert dynamic strings
+// abuf will server as our buffer.
+// It consists of a pointer to our buffer in memory, and a length.
+// ABUF_INIT serves as the constructor for abuf structs.
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT                                                              \
+  { NULL, 0 }
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  // create a new memory location with realloc.
+  // ab->b means, access the property b from the structure ab
+  // ab-> len + len means, access the property len from the structure ab and add
+  // len to it. The len that was passed in to abappend
+  //
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL)
+    return; // realloc returns NULL if an error occured and a new memory could
+            // not be allocated.
+  // Copy the content of s into &new[ab->len] (the last position of the newly
+  // allocated memory)
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) { free(ab->b); }
+
 void editorDrawRows() {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    write(STDOUT_FILENO, "~", 1);
+
+    if (y < E.screenrows - 1) {
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 /*** input ***/
@@ -92,12 +156,18 @@ void editorProcessKeypress() {
 }
 
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  // \x1b = 27 (in decimal) = The escape charactor. it is followed by a [
+  // charactor. These are known as escape sequences. With 2J we ae passing the
+  // argument of 2 to the command J. J is the command to clear screen. H is the
+  // command to reaturn cursor position. All these are from the VT100. Check the
+  // VT100 User Guide for the complete documentation of each escape sequence.
+  write(STDOUT_FILENO, "\x1b[2J", 4); // Clear Screen
+  write(STDOUT_FILENO, "\x1b[H",
+        3); // Return cursor to the top-most-left corner of the screen.
 
-  editorDrawRows();
+  editorDrawRows(); // Draw the ~ char on every row
 
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  write(STDOUT_FILENO, "\x1b[H", 3); // Return cursor to top-most-left corner
 }
 
 /*** init */
